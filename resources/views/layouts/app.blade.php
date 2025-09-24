e87d062d4cfc6bc26dfdbfe4fa07a0242b3d7bbe
Felipe Iglesias <felipeiglesiasdev@gmail.com>
2025-09-23 22:22:36 -0300

    projeto base!!

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,28 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toma Cupom</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="{{ asset('css/base.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">    
 </head>
 <body>
-
-    {{-- Cabeçalho global --}}
     <x-cabecalho />
-
-    {{-- Conteúdo dinâmico --}}
-    <main>
+    <main class="main-content">
         @yield('content')
     </main>    
-
-    {{-- Rodapé global --}}
     <x-rodape />
-
-    <script src="{{ asset('js/header.js') }}"></script>
-
 </body>
 </html>
